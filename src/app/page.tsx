4d4e5882e3a56fb45d7605797d69d2e7b0866529
Hamza Eraoui <hamzaeraoui2000@gmail.com>
2025-10-16 12:59:29 +0000

    put the blog section before Projects in the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,13 @@ import { Projects } from "@/components/sections/projects";
 import { Blog } from "@/components/sections/blog";
 import { Services } from "@/components/sections/services";
 import { Contact } from "@/components/sections/contact";
-import { OpenSource } from "@/components/sections/open-source";
 
 export default function Home() {
   return (
     <div className="container mx-auto max-w-5xl px-4 md:px-8">
       <Hero />
-      <Projects />
       <Blog />
+      <Projects />
       <Services />
       <Contact />
     </div>
