6a34623fa6e385da1aa6218747356146a3de75a6
Hamza Eraoui <hamzaeraoui2000@gmail.com>
2025-11-03 14:01:18 +0000

    No put the logo between © 2025 Hamza Eraoui.  and All rights reserved.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -9,7 +9,8 @@ export function Footer() {
     <footer className="w-full border-t border-border/40">
       <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
-          <div className="flex items-center gap-2">
+          <p className="flex items-center gap-2 text-center text-sm leading-loose text-muted-foreground md:text-left">
+            <span>© {new Date().getFullYear()} Hamza Eraoui.</span>
             <Image
               src="/logos/happy-face-bg-rm.png"
               alt="Logo"
@@ -17,10 +18,8 @@ export function Footer() {
               height={20}
               className="h-5 w-5"
             />
-            <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-              © {new Date().getFullYear()} Hamza Eraoui. All rights reserved.
-            </p>
-          </div>
+            <span>All rights reserved.</span>
+          </p>
         </div>
         <div className="flex items-center space-x-2">
           {personalData.contact.social.map((social) => (
